0482c9f9bfc0e55791f619bbb70162e7f10dc9ae
Michael Edwards <michael.david.edwards7@gmail.com>
2026-03-15 19:39:06 -0400

    Fix missing using directive for Screen in MonitorManager

--- NovaLog.Avalonia/Services/MonitorManager.cs
+++ NovaLog.Avalonia/Services/MonitorManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Platform;
 using Dock.Model.Core;
 using NovaLog.Avalonia.Docking;
 using NovaLog.Avalonia.ViewModels;
